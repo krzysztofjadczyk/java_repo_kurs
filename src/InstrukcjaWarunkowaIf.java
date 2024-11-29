51b0e0eefe75f53d1fc391defb43b4b0cf0c2761
krzysztofjadczyk <krzysztof.jadczyk@gmail.com>
2024-11-29 14:06:45 +0100

    State after 76

--- src/InstrukcjaWarunkowaIf.java
+++ src/InstrukcjaWarunkowaIf.java
@@ -2,9 +2,12 @@
 public class InstrukcjaWarunkowaIf {
     public static void main(String[] args) {
         float a = 10;
-        float b = 12;
+        float b = 0;
         if (b!=0) {
             System.out.println("Wynik dzielenia to: " + (a/b));
+        }
+        else {
+            System.out.println("Podaj inną liczbę różną od 0");
         }
     }
 }
