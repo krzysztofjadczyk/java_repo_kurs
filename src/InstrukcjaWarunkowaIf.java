609a3d8cc554874ad60e5cb3292d71d63c735da6
krzysztofjadczyk <krzysztof.jadczyk@gmail.com>
2024-11-29 13:29:14 +0100

    State after 75

---
+++ src/InstrukcjaWarunkowaIf.java
@@ -0,0 +1,10 @@
+// instrukcje wrunkowe pozwalają nam określać, które części kodu mają zostać wykonane, gdy zostanie spełniony warunek
+public class InstrukcjaWarunkowaIf {
+    public static void main(String[] args) {
+        float a = 10;
+        float b = 12;
+        if (b!=0) {
+            System.out.println("Wynik dzielenia to: " + (a/b));
+        }
+    }
+}
