bdc58c16a9f79b045ed36d87770f04349089251f
krzysztofjadczyk <krzysztof.jadczyk@gmail.com>
2024-12-02 12:25:50 +0100

    State after 95

--- src/Metody.java
+++ src/Metody.java
@@ -8,6 +8,21 @@ public class Metody {
         System.out.println("Wartośc to: " + result);
     }
 
+    public int pobierzWynik() {
+        int result = 0;
+        for (int i=0; i<100; i++) {
+            result = result+i;
+        }
+        //System.out.println("Wartośc to: " + result);
+        return result;
+    }
+
+    public int wyliczWynik(int a, int b) {
+        int result = a+b;
+        //System.out.println("Wartośc to: " + result);
+        return result;
+    }
+
     public void sum() {
         int firstNumber = 0;
         int secondNumber = 2;
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -4,5 +4,13 @@ public class MetodyTest {
         metody.policzWynik();
 
        // int result = metody.policzWynik(); // błąd bo metoda void nie zwraca nic
+
+        int result = metody.pobierzWynik();
+
+        System.out.println("Zwócony wynik przez metodę: " + result);
+
+        int result2 = metody.wyliczWynik(5, 6);
+        System.out.println("Zwócony wynik przez metodę wyliczWynik: " + result2);
+
     }
 }
