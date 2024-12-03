fdf290114ec87cdf0bf5c2463f48969781d2cb85
krzysztofjadczyk <krzysztof.jadczyk@gmail.com>
2024-12-03 11:37:35 +0100

    State after 110

---
+++ src/MethodNameTest.java
@@ -0,0 +1,11 @@
+public class MethodNameTest {
+
+    public static void main(String[] args) {
+        MethodName methodName = new MethodName();
+        methodName.add();
+        methodName.add(1);
+        methodName.add("hello");
+        methodName.add(5, 8);
+        methodName.add(3,6,9);
+    }
+}
